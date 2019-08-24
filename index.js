63637bc7eef4810cbe5c9dd0f8068655952a45a7
chilbert <chase@chasehilbert.com>
2019-08-24 11:10:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let var1 = 2
+let var2 = 31
+
+let multiply = var1 * var2;
+
+let random = (Math.random()) + 1;
+
+let mod = 16 % 6;
+
+let max = Math.max(1, 5, 20, 14, 16);
