94165db6c002eaabd4331fb2a6c1dfda876dd84f
blitzparanoia <bluemilk44@gmail.com>
2019-08-25 19:41:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here
+
+const var1 = 2
+const var2 = 31
+const multiply = var1 * var2
+const random = Math.floor(Math.random() * 10 + 1)
+const mod = 20 % 16
+const max = Math.max(1, 20, 3)
