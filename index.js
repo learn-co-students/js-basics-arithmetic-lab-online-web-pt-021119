8a078fb8a9452049c3fc1c2181e6a452f108ee51
Ryan Wolfe <ryan.d.wolfe@gmail.com>
2019-08-26 15:40:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here
+let var1 = 1;
+let var2 = 62;
+let random = Math.floor(Math.random()) + 1;
+let max = Math.max(1, 2, 3, 4, 20);
+let multiply = var1 * var2;
+let mod = 16 % 6
